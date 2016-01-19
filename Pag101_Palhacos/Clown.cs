eb1e82a24e88db5b12116bf0062bf83a1b2c898a
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2016-01-19 19:09:28 -0200

    Concluído

---
+++ Pag101_Palhacos/Clown.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pag101_Palhacos
+{
+    class Clown
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+
+        public void TalkAboutYourselft()
+        {
+            MessageBox.Show("My name is "
+                    + Name + " and I'am "
+                    + Height + " inches tall.");
+        }
+    }
+}
---
+++ Pag101_Palhacos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pag101_Palhacos
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
