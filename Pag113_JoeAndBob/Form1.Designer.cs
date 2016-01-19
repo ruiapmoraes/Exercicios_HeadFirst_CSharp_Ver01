d5d5dade284b37ffbb73c232ca52c1dc846412c5
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2016-01-19 20:23:29 -0200

    Exercicio pausado.

--- Pag113_JoeAndBob/Form1.Designer.cs
+++ Pag113_JoeAndBob/Form1.Designer.cs
@@ -28,20 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.joeCashLabel = new System.Windows.Forms.Label();
+            this.bobsCashLabel = new System.Windows.Forms.Label();
+            this.banckCashLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // joeCashLabel
+            // 
+            this.joeCashLabel.AutoSize = true;
+            this.joeCashLabel.Location = new System.Drawing.Point(12, 9);
+            this.joeCashLabel.Name = "joeCashLabel";
+            this.joeCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.joeCashLabel.TabIndex = 0;
+            this.joeCashLabel.Text = "label1";
+            // 
+            // bobsCashLabel
+            // 
+            this.bobsCashLabel.AutoSize = true;
+            this.bobsCashLabel.Location = new System.Drawing.Point(12, 29);
+            this.bobsCashLabel.Name = "bobsCashLabel";
+            this.bobsCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bobsCashLabel.TabIndex = 1;
+            this.bobsCashLabel.Text = "label2";
+            // 
+            // banckCashLabel
+            // 
+            this.banckCashLabel.AutoSize = true;
+            this.banckCashLabel.Location = new System.Drawing.Point(12, 51);
+            this.banckCashLabel.Name = "banckCashLabel";
+            this.banckCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.banckCashLabel.TabIndex = 2;
+            this.banckCashLabel.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Give 10$ to Joe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Receive 5$ from Bob";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 221);
+            this.ClientSize = new System.Drawing.Size(290, 182);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.banckCashLabel);
+            this.Controls.Add(this.bobsCashLabel);
+            this.Controls.Add(this.joeCashLabel);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Diversão com Joe e Bob";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label joeCashLabel;
+        private System.Windows.Forms.Label bobsCashLabel;
+        private System.Windows.Forms.Label banckCashLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Pag113_JoeAndBob/Form1.cs
+++ Pag113_JoeAndBob/Form1.cs
@@ -12,9 +12,50 @@ namespace Pag113_JoeAndBob
 {
     public partial class Form1 : Form
     {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+
+        public void UpdateForm()
+        {
+            joeCashLabel.Text = joe.Name + " has $" + joe.Cash;
+            bobsCashLabel.Text = bob.Name + "has $" + bob.Cash;
+            banckCashLabel.Text = "The bank has $" + bank;
+        }
+
         public Form1()
         {
             InitializeComponent();
+            // inicialize joe e bob aqui!
+
+            bob = new Guy();
+            bob.Name = "Bob";
+            bob.Cash = 100;
+
+            joe = new Guy();
+            joe.Name = "Joe";
+            joe.Cash = 50;
+
+            UpdateForm();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bank >=10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("The bank is out of money");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
         }
     }
 }
