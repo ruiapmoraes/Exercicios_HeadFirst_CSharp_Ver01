57d273f79193c5ad0ce6a1572310786e1f5da55a
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2016-01-19 20:04:06 -0200

    inclusão da classe.

---
+++ Pag113_JoeAndBob/Guy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pag113_JoeAndBob
+{
+    class Guy
+    {
+        public string  Name { get; set; }
+        public int Cash { get; set; }
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= Cash && amount >0)
+            {
+                Cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(
+                        "I don't have enough cash to give you " + amount,
+                        Name + " says...");
+                return 0;
+            }
+        }
+
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(amount + " isn't an amount I'll take",
+                        Name + " says...");
+                return 0;
+            }
+        }
+    }
+}
