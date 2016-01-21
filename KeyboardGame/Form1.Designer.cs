940880cf949fd5a34e21337944d47381457cefb9
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2016-01-21 12:16:50 -0200

    Adicionando formulário

--- KeyboardGame/Form1.Designer.cs
+++ KeyboardGame/Form1.Designer.cs
@@ -34,7 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 136);
+            this.ClientSize = new System.Drawing.Size(856, 131);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hit the Key!";
