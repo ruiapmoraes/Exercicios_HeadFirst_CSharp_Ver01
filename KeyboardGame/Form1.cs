16b3d7a46de91d73588db46103e7cc3c80a5d1fa
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2016-01-21 12:45:31 -0200

    Adicionando classes Stats

--- KeyboardGame/Form1.cs
+++ KeyboardGame/Form1.cs
@@ -12,9 +12,23 @@ namespace KeyboardGame
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        Stats stats = new Stats();
         public Form1()
-        {
+        {            
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Adicione uma tecla aleatória a Listbox
+            listBox1.Items.Add((Keys)random.Next(60, 90));
+            if (listBox1 .Items .Count >7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game Over");
+                timer1.Stop();
+            }
         }
     }
 }
