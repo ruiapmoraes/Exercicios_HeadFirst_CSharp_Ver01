efef62effcbc1f74ac6ccb7c9bdd90854846d850
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2016-01-27 18:05:07 -0200

    pag 257 - finalizando forms

--- Pag259_GerenciadorColmeia/Form1.Designer.cs
+++ Pag259_GerenciadorColmeia/Form1.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNextShift = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.workerBeeJob = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
+            this.btnAssignJob = new System.Windows.Forms.Button();
+            this.txtReport = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAssignJob);
+            this.groupBox1.Controls.Add(this.shifts);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.workerBeeJob);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 90);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tarefas das abelhas atribuídas";
+            // 
+            // btnNextShift
+            // 
+            this.btnNextShift.Location = new System.Drawing.Point(246, 14);
+            this.btnNextShift.Name = "btnNextShift";
+            this.btnNextShift.Size = new System.Drawing.Size(92, 88);
+            this.btnNextShift.TabIndex = 1;
+            this.btnNextShift.Text = "Work the next shift";
+            this.btnNextShift.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Worker bee job";
+            // 
+            // workerBeeJob
+            // 
+            this.workerBeeJob.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.workerBeeJob.FormattingEnabled = true;
+            this.workerBeeJob.Location = new System.Drawing.Point(10, 37);
+            this.workerBeeJob.Name = "workerBeeJob";
+            this.workerBeeJob.Size = new System.Drawing.Size(121, 21);
+            this.workerBeeJob.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Shifts";
+            // 
+            // shifts
+            // 
+            this.shifts.Location = new System.Drawing.Point(147, 37);
+            this.shifts.Name = "shifts";
+            this.shifts.Size = new System.Drawing.Size(72, 20);
+            this.shifts.TabIndex = 3;
+            // 
+            // btnAssignJob
+            // 
+            this.btnAssignJob.Location = new System.Drawing.Point(7, 65);
+            this.btnAssignJob.Name = "btnAssignJob";
+            this.btnAssignJob.Size = new System.Drawing.Size(124, 23);
+            this.btnAssignJob.TabIndex = 4;
+            this.btnAssignJob.Text = "Assign this job to a bee";
+            this.btnAssignJob.UseVisualStyleBackColor = true;
+            // 
+            // txtReport
+            // 
+            this.txtReport.Location = new System.Drawing.Point(13, 109);
+            this.txtReport.Multiline = true;
+            this.txtReport.Name = "txtReport";
+            this.txtReport.Size = new System.Drawing.Size(325, 190);
+            this.txtReport.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(355, 311);
+            this.Controls.Add(this.txtReport);
+            this.Controls.Add(this.btnNextShift);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerenciador de Colméias";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAssignJob;
+        private System.Windows.Forms.NumericUpDown shifts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox workerBeeJob;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNextShift;
+        private System.Windows.Forms.TextBox txtReport;
     }
 }
 
