ed1ec15ba47482a50d465328c48607d5ed4736b7
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2016-01-27 14:46:19 -0200

    commit pag 244

---
+++ Pag_244_Heranca/Locksmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pag_244_Heranca
+{
+    class Locksmith
+    {
+        private string writtenDownCombination = null;
+
+        public void OpenSafe(Safe safe, Owner owner)
+        {
+            safe.PickLock(this);
+            Jewels safeContents = safe.Open(writtenDownCombination);
+            ReturnContents(safeContents, owner);
+        }
+
+        private void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void WriteDownCombination(string safeCombination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
